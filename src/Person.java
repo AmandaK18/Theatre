5a6ee5aab651c059ae8adf2c511f217e112b5454
AmandaK18 <kalani.20221254@iit.ac.lk>
2024-03-08 18:10:24 +0530

    person

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+//Author's name: Kiringodage Amanda
+//Student ID:w1956091
+import java.util.Scanner;
+public class Person {
+    String name;
+    String surname;
+    String email;
+
+    public Person(String name,String surname,String email){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+}
